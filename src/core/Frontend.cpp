3c8cdd57723eace9e70bbe1019aeb230c006b8c7
eray orçunus <erayorcunus@gmail.com>
2020-06-25 16:02:32 +0300

    Restored beta police system(disabled), fixes from miami, debug info for CFO crash

--- src/core/Frontend.cpp
+++ src/core/Frontend.cpp
@@ -1216,8 +1216,10 @@ CMenuManager::Draw()
 							rightText = option.drawFunc(&isOptionDisabled);
 						}
 					}
-				} else
+				} else {
+					debug("screen:%d - totalCo: %d, coId: %d, coScreen:%d, coOption:%d\n", m_nCurrScreen, numCustomFrontendOptions, aScreens[m_nCurrScreen].m_aEntries[i].m_TargetMenu, option.screen, option.screenOptionOrder);
 					assert(0 && "Custom frontend options is borked");
+				}
 
 				break;
 #endif
@@ -4774,8 +4776,10 @@ CMenuManager::ProcessButtonPresses(void)
 						} else if (option.type == FEOPTION_GOBACK) {
 							goBack = true;
 						}
-					} else
+					} else {
+						debug("screen:%d - totalCo: %d, coId: %d, coScreen:%d, coOption:%d\n", m_nCurrScreen, numCustomFrontendOptions, aScreens[m_nCurrScreen].m_aEntries[m_nCurrOption].m_TargetMenu, option.screen, option.screenOptionOrder);
 						assert(0 && "Custom frontend options are borked");
+					}
 
 					break;
 #endif
@@ -4997,8 +5001,10 @@ CMenuManager::ProcessButtonPresses(void)
 					}
 					DMAudio.PlayFrontEndSound(SOUND_FRONTEND_MENU_SETTING_CHANGE, 0);
 				}
-				else
+				else {
+					debug("screen:%d - totalCo: %d, coId: %d, coScreen:%d, coOption:%d\n", m_nCurrScreen, numCustomFrontendOptions, aScreens[m_nCurrScreen].m_aEntries[m_nCurrOption].m_TargetMenu, option.screen, option.screenOptionOrder);
 					assert(0 && "Custom frontend options are borked");
+				}
 
 				break;
 #endif
